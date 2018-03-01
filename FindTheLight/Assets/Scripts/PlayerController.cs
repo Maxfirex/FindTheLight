8a08fd61673205acc6ea25a7848c6b5842dc54c7
Kristian-PC\Kristian <kristian.leiner@yahoo.com>
2018-03-01 21:03:16 +0100

    Player controls
    Added movement and look functionality

---
+++ FindTheLight/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10f;
+
+    void Start () {
+        //hides cursor when playing
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update () {
+        //value for moving forwards and backwards
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        //value for moving left and right
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(horizontal, 0, vertical);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            //displays cursor on escape
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+	}
+}
