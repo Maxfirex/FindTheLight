0ac1ba5598e76898e9ca6036f81f2beb04aaa291
Kristian-PC\Kristian <kristian.leiner@yahoo.com>
2018-03-06 23:12:11 +0100

    Weapon controls
    Added weapon fire rate and shooting sound

--- FindTheLight/Assets/Scripts/GunController.cs
+++ FindTheLight/Assets/Scripts/GunController.cs
@@ -4,8 +4,11 @@ public class GunController : MonoBehaviour {
 
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 5f;
 
     GameObject cam;
+
+    private float lastShot = 0f;
 
     void Start ()
     {
@@ -15,9 +18,22 @@ public class GunController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetMouseButton(0))
         {
-            Shoot();
+            if (Time.time > 1/fireRate + lastShot)
+            {
+                Debug.Log("Called");
+                Shoot();
+
+                FindObjectOfType<AudioManager>().Play("shoot");
+
+                lastShot = Time.time;
+            }
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            FindObjectOfType<AudioManager>().Stop("shoot");
         }
 	}
 
@@ -27,13 +43,12 @@ public class GunController : MonoBehaviour {
 
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
-
             EnemyController enemy = hit.transform.GetComponent<EnemyController>();
 
             if (enemy != null)
             {
                 enemy.TakeDamage(damage);
+                Debug.Log(enemy.health);
             }
         }
     }
